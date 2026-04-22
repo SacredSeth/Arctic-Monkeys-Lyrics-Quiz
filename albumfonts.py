cbd5c05541129ca0a94e0e1ad1674e1ec5c9290a
SacredSeth <141595269+SacredSeth@users.noreply.github.com>
2026-04-22 14:53:41 +1200

    albums and their fonts

---
+++ albumfonts.py
@@ -0,0 +1,15 @@
+album_list = ["Whatever People Say I Am, That's What I'm Not",
+              "Favourite Worst Nightmare",
+              "Humbug",
+              "Suck It and See",
+              "AM",
+              "Tranquility Base Hotel & Casino"]
+
+file_list = ["Fonts\AutourOne-Regular.ttf",
+             "Fonts\JotiOne-Regular.ttf",
+             "Fonts\RobotoSlab-Regular.ttf",
+             "Fonts\SpecialGothicExpandedOne-Regular.ttf",
+             "Fonts\LuckiestGuy-Regular.ttf",
+             "Fonts\Sarpanch-Regular.ttf"]
+font_families = ["Autour One", "Joti One", "Roboto Slab",
+                 "Special Gothic Expanded One", "Luckiest Guy", "Sarpanch"]
